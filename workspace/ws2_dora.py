007061d5b963de537e49627706f6b896fe65180b
zory233 <yzhanglo@connect.ust.hk>
2024-02-13 16:53:35 -0600

    fix skimage

--- workspace/ws2_dora.py
+++ workspace/ws2_dora.py
@@ -1,6 +1,6 @@
 # import the necessary packages
 from skimage.feature import peak_local_max
-from skimage.morphology import watershed
+from skimage.segmentation import watershed
 from scipy import ndimage
 from imutils import perspective
 import numpy as np
@@ -22,7 +22,7 @@ class vis_obj(object):
         self.contour = contour
         self.min_rect = rect
         self.bounding_rect = None
-        self.length = None # in pixels. 
+        self.length = None # in pixels.
         self.height = None # in pixels.
         self.area = None # in pixels.
         self.allPointsOfContour = None
@@ -34,7 +34,7 @@ class vis_obj(object):
         self.rightmost = None
         self.my_image = the_image
         self.my_pairs = None
-    
+
     # function to fill the rest of my fields.
     def fill_fields(self):
         # fill corners of bounding rect.
@@ -51,8 +51,8 @@ class vis_obj(object):
         cv2.drawContours(mask,[self.contour],0,255,-1)
         self.allPointsOfContour = cv2.findNonZero(mask)
         # get the midpoint, and the top, bottom, left, and rightmost points of the contour.
-        self.midpoint = (self.min_rect[1][0]+(self.min_rect[0][0]/2), self.min_rect[0][1]+(self.min_rect[1][1]/2)) # midpoint of the minimum bounding rectangle. 
-        self.position = (self.bounding_rect[0]+(self.bounding_rect[2]/2), self.bounding_rect[1]+(self.bounding_rect[3]/2)) # midpoint of bounding rectangle. 
+        self.midpoint = (self.min_rect[1][0]+(self.min_rect[0][0]/2), self.min_rect[0][1]+(self.min_rect[1][1]/2)) # midpoint of the minimum bounding rectangle.
+        self.position = (self.bounding_rect[0]+(self.bounding_rect[2]/2), self.bounding_rect[1]+(self.bounding_rect[3]/2)) # midpoint of bounding rectangle.
         self.leftmost = tuple(self.contour[self.contour[:,:,0].argmin()][0])
         self.rightmost = tuple(self.contour[self.contour[:,:,0].argmax()][0])
         self.topmost = tuple(self.contour[self.contour[:,:,1].argmin()][0])
@@ -65,12 +65,12 @@ class pairs(object):
         self.object2 = object2
 
 # funciton to find the objects in an image using contours and watershedding. It takes as input the name of the file of the image, and returns an array of the limited bounding rectangle of all detected objects.
-def find_objs(image_name): 
+def find_objs(image_name):
     # load the image as color (for later use with watershed() funciton), and convert to grayscale.
     image = cv2.imread(image_name,1)
     gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
     #cv2.imshow("Input", image)
-    #cv2.waitKey(0)    
+    #cv2.waitKey(0)
     ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
     # noise removal.
     kernel = np.ones((3,3),np.uint8)
@@ -91,10 +91,10 @@ def find_objs(image_name):
     markers[unknown==255] = 0
     # apply watershed.
     markers2 = cv2.watershed(image,markers)
-    # find the contours of each of the individual shapes. 
+    # find the contours of each of the individual shapes.
     # first, find out how many individual shapes you have (you can do this by looking at how many unique values there are in the markers2 matrix, minus 2 (element encode borders (-1), the other background (1)).
     unique_values = np.unique(markers2)
-    # second, create a masked image for each figure that only includes that figure (i.e., create a copy of markers2, and set all values that don't code for a specific individual element (e.g., '2'), to background(=1)). Put all the masked images into a single array. 
+    # second, create a masked image for each figure that only includes that figure (i.e., create a copy of markers2, and set all values that don't code for a specific individual element (e.g., '2'), to background(=1)). Put all the masked images into a single array.
     masked_images = []
     for value in unique_values:
         # create a copy of the thresholded image, set all values that code background in item to grey-scale black (0). Skip any values lower than 1 (the value you set the background to).
@@ -102,7 +102,7 @@ def find_objs(image_name):
             thresh_image_copy = thresh.copy()
             thresh_image_copy[markers2 != value] = 0
             masked_images.append(thresh_image_copy)
-    # third, get the minimum bounding rectangle for the element element in each of the masked images. Put those bounding rectangles into an array. 
+    # third, get the minimum bounding rectangle for the element element in each of the masked images. Put those bounding rectangles into an array.
     image_objects = []
     for item in masked_images:
         # get the minimum bounding rectagle for the object in that image.
@@ -110,7 +110,7 @@ def find_objs(image_name):
         rect = cv2.minAreaRect(contours[0])
         object1 = [contours[0], rect]
         image_objects.append(object1)
-    
+
     # done.
     return image_objects
 
@@ -119,12 +119,12 @@ def find_objs(image_name):
 # cv2.imshow("fixed?", image)
 # cv2.waitKey(0)
 
-# function to take in an image, identify objects via edges, and create visual objects from each bounded edge. 
+# function to take in an image, identify objects via edges, and create visual objects from each bounded edge.
 def get_vis_objs(image):
     image_objects = find_objs(image)
     image = cv2.imread(image,1)
     items = []
-    # create the the_image object. 
+    # create the the_image object.
     my_image = the_image(image)
     # put all the objects (as vis_obj objects) into the image object.
     for item in image_objects:
@@ -133,7 +133,7 @@ def get_vis_objs(image):
     # for each object in the image, fill in the fields.
     for item in items:
         item.fill_fields()
-    # return the array of visual objects. 
+    # return the array of visual objects.
     return items
 
 # function to round to nearest x place.
@@ -141,10 +141,10 @@ def roundX(number, x):
     value = math.ceil(number/float(x)) * x
     return int(value)
 
-# function to create DORA objects from the visual objects. The DORA objects have as features, ... 
+# function to create DORA objects from the visual objects. The DORA objects have as features, ...
 prototype={'name': 'non_exist', 'RBs': [{'pred_name': 'non_exist', 'pred_sem': [], 'higher_order': False, 'object_name': 'obj', 'object_sem': [], 'P': 'non_exist'}], 'set': 'memory', 'analog': None, 'contour': None}
 def make_DORA_objs(items):
-    # for each item, make a DORA object with features ... 
+    # for each item, make a DORA object with features ...
     objs = []
     analog_counter = 0
     for item in items:
@@ -152,7 +152,7 @@ def make_DORA_objs(items):
         for vis_obj in item:
             # create a copy of the sym_file object prototype.
             new_sym_dict = copy.deepcopy(prototype)
-            # fill in the analog for the object. 
+            # fill in the analog for the object.
             new_sym_dict['analog'] = analog_counter
             # create a name for the object.
             new_sym_dict['RBs'][0]['object_name'] = 'obj'+str(len(objs))+str(random.random())
@@ -169,32 +169,32 @@ def make_DORA_objs(items):
             new_sym_dict['RBs'][0]['object_sem'].append(['total_ext'+str(roundX(vis_obj.area, 10)), 1, 'total_ext', roundX(vis_obj.area, 10), 'value'])
             # add the contour to the sym object dictionary.
             #new_sym_dict['contour'] = vis_obj.contour
-            # add the new sym object to objs. 
+            # add the new sym object to objs.
             objs.append(new_sym_dict)
         # update analog_counter
         analog_counter+=1
-    # return list of DORA objects with features. 
+    # return list of DORA objects with features.
     return objs
 
 
 #########################################################################################
-# MAIN BODY. 
+# MAIN BODY.
 #########################################################################################
 
-# make a list of all the pictures you want to procoess. For this project, it's a bunch of images called 'shapesn.jpb', where n is a number starting from 1 and going to the number of images to be processed. 
+# make a list of all the pictures you want to procoess. For this project, it's a bunch of images called 'shapesn.jpb', where n is a number starting from 1 and going to the number of images to be processed.
 num_pics = 100
 image_list = []
 for i in range(num_pics):
     img_name = 'shapes'+str(i+1)+'.jpg'
     image_list.append(img_name)
 
-# for every picture in image_list, create vis_objs of objects in the image, and put them in a list. Add the list of vis_objs to a master list of all lists of objects from all images, master_list. 
+# for every picture in image_list, create vis_objs of objects in the image, and put them in a list. Add the list of vis_objs to a master list of all lists of objects from all images, master_list.
 master_list = []
 for pic in image_list:
     items = get_vis_objs(pic)
     master_list.append(items)
 
-# create DORA objects for all objects in the master_list. 
+# create DORA objects for all objects in the master_list.
 objs = make_DORA_objs(master_list)
 
 # now write all the sym_objs to a file.
